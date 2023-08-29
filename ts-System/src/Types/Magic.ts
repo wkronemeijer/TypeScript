6afb42b102d5752cb43d892274fff705eeae576e
Wessel Kronemeijer <wessel32@gmail.com>
2023-08-29 14:29:53 +0200

    Remove unused, expand doc comments

--- ts-System/src/Types/Magic.ts
+++ ts-System/src/Types/Magic.ts
@@ -1,21 +1,29 @@
-
 // Arcane type magic
-// from https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type
-/** Converts a union of types into a intersection of those types. */
-export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
+// From https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type
+/** 
+ * Converts a union of types into a intersection of those types. 
+ * 
+ * Useful to create objects that can handle every possible case.
+ */
+export type UnionToIntersection<U> = (
+    U extends any ? (k: U) => void : never
+) extends (
+    (k: infer I) => void
+) ? I : never;
 
-// From https://github.com/microsoft/TypeScript/pull/40336
-export type Methods<T> = { [P in keyof T as T[P] extends Function ? P : never]: T[P] };
-export type Fields<T>  = { [P in keyof T as T[P] extends Function ? never : P]: T[P] };
-
-
-// https://github.com/microsoft/TypeScript/issues/28508#issuecomment-775742020
+// From https://github.com/microsoft/TypeScript/issues/28508#issuecomment-775742020
+/**
+ * Expands a type to its normal form.
+ * Doesn't change the actual shape of the object, but 
+ * TS does not see that and will require a cast.
+ * 
+ * Useful for experimenting with type functions.
+ */
 export type ExpandType<T> = {} & { [P in keyof T]: T[P] };
 
-export type ReplaceThis<T, NewThis> = {
-    [P in keyof T]: 
-    T[P] extends (...args: any) => any 
-    ? (this: NewThis, ...args: Parameters<T[P]>) => ReturnType<T[P]>
-    : T[P];
-}
+// From https://github.com/microsoft/TypeScript/issues/29729#issuecomment-554669605
+/**
+ * Adds autocomplete hints to a string variable.
+ */
+export type HintedString<THint extends string> = THint | (string & {});
