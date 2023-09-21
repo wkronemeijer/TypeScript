b8d128805e0b81684acc8a25ec45df77f5fba6e6
Wessel Kronemeijer <wessel32@gmail.com>
2023-09-21 20:22:22 +0200

    Add Any as mixin base class

--- ts-System/src/Types/Mixins.ts
+++ ts-System/src/Types/Mixins.ts
@@ -2,8 +2,15 @@
 // from https://mariusschulz.com/blog/mixin-classes-in-typescript
 // Thanks, Marius!
 /** Generic shape of constructor of T instances. */
-export type Constructor<T = {}> = new (...args: any[]) => T;
+export type Constructor<T = any> = new (...args: any[]) => any;
 
 // Mixins don't like extending null, so we add 1 layer of indirection.
 /** A object type with no members. Useful for mixins. */
 export class Null extends null { }
+
+/**
+ * A class with no properties or methods.
+ * 
+ * Useful as a root class for mixins.
+ */
+export class Any {}
