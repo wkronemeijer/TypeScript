94050adc19dfe63a53b8b6882f214ee3857d273c
Wessel Kronemeijer <wessel32@gmail.com>
2023-10-23 20:11:24 +0200

    Remove private function

--- ts-System/src/Types/Satisfies.ts
+++ ts-System/src/Types/Satisfies.ts
@@ -1,6 +1,5 @@
 import { AssertionFunction, requires } from "../Assert";
 import { identity } from "../Data/Function";
-import { InstanceOwner_declare } from "./InstanceOwner";
 
 /** 
  * For regular use, TS 4.9 lets you use the `satisfies` operator directly. 
