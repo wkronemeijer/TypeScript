250ed1ca026479bfb7f675145ed8ab027599b253
Wessel Kronemeijer <wessel32@gmail.com>
2023-10-26 21:42:06 +0200

    Copy readLine to node commons

---
+++ ts-System_Node/src/IO/Terminal/ReadLine.ts
@@ -0,0 +1,21 @@
+const { stdin, stdout } = process;
+const bufferEncoding: BufferEncoding = "utf-8";
+
+/** 
+ * Reads a single line from `stdin`. 
+ * 
+ * If you are creating a interactive CLI app, 
+ * look into the `readline` node module.
+ * */
+export function readLine(prompt?: string): Promise<string> {
+    return new Promise(resolve => {
+        if (prompt !== undefined) {
+            stdout.write(prompt);
+        }
+        stdin.resume();
+        stdin.once("data", buffer => {
+            stdin.pause();
+            resolve(buffer.toString(bufferEncoding));
+        });
+    });
+}
