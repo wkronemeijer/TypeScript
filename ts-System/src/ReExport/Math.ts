22af1a0a3b7f87a638408bb0f2df7304267df569
Wessel Kronemeijer <wessel32@gmail.com>
2023-12-19 16:15:58 +0100

    Re-export Math as a module

---
+++ ts-System/src/ReExport/Math.ts
@@ -0,0 +1,24 @@
+const {
+    random,
+    sign, abs,
+    min, max,
+    pow, sqrt,
+    floor, ceil, trunc, round,
+    sin, cos, tan, atan2,
+    fround, imul,
+    log: ln, log10, log2,
+    PI: pi,
+} = Math;
+
+export const tau = 2 * pi;
+export {
+    random,
+    sign, abs,
+    min, max,
+    pow, sqrt,
+    floor, ceil, trunc, round,
+    sin, cos, tan, atan2,
+    fround, imul,
+    ln, log10, log2,
+    pi,
+};
