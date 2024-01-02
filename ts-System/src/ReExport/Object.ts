0e699089f546f8262d589236468eebf309e5b07a
Wessel Kronemeijer <wessel32@gmail.com>
2024-01-02 20:33:56 +0100

    Re-export Object properties

---
+++ ts-System/src/ReExport/Object.ts
@@ -0,0 +1,13 @@
+const {
+    create, 
+    defineProperty, hasOwn, 
+    freeze, 
+    assign, entries,
+} = Object;
+
+export {
+    create, 
+    defineProperty, hasOwn, 
+    freeze, 
+    assign, entries,
+}
