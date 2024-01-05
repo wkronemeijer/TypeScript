b911c5f418831760f528d75b80bd960d4d79ac78
Wessel Kronemeijer <wessel32@gmail.com>
2024-01-05 15:02:46 +0100

    Simplify serializable API

---
+++ ts-System/src/Serialization/Json.ts
@@ -0,0 +1,44 @@
+import { Newtype } from "../Modules.generated";
+
+const { stringify, parse } = JSON;
+
+export type JsonArray  = readonly JsonValue[];
+export type JsonObject = { readonly [s: string]: JsonValue; };
+export type JsonValue  = (
+    | null
+    | boolean
+    | number
+    | string
+    | JsonArray
+    | JsonObject
+);
+
+// Note: these /can/ appear in the input, but never in the output.
+export interface StringifyableObject {
+    toJSON(): JsonValue;
+}
+
+export type JsonInputValue = (
+    | JsonValue 
+    | StringifyableObject
+);
+
+export type JsonOutputValue = JsonValue;
+
+export type Json = Newtype<string, "Json">;
+
+export interface JsonReplacer {
+    (this: object, key: string, value: unknown): unknown;
+}
+
+export function stringifyJson(value: unknown, replacer?: JsonReplacer, space?: string | number): Json {
+    return stringify(value, replacer, space) as Json;
+}
+
+export interface JsonReviver {
+    (this: object, key: string, value: unknown): unknown;
+}
+
+export function parseJson(json: string, reviver?: JsonReviver): unknown {
+    return parse(json, reviver);
+}
