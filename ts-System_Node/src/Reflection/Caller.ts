6cbb3a5a261d769844d0d2f189b2d49c688510bb
Wessel Kronemeijer <wessel32@gmail.com>
2024-01-14 16:34:05 +0100

    Cleanup

--- ts-System_Node/src/Reflection/Caller.ts
+++ ts-System_Node/src/Reflection/Caller.ts
@@ -1,4 +1,5 @@
 // Analogous to System.Runtime.CompilerServices
+import { fileURLToPath } from "url";
 
 import { panic } from "@wkronemeijer/system";
 
@@ -11,39 +12,37 @@ type CallSite           = CallStack[number];
 
 /** Returns the callsite of "your caller". 
  * 
- * Position parameter is for helper functions. Direct calls should use 2 (the default), helper functions should use 3.
+ * Position parameter is for helper functions. Direct calls should use 0 (the default), helper functions should use 1.
  */
-export function getCallSite(
-    /** 
-    [0] = getCallSite  
-    [1] = the caller (who wants to know who called /him/)
-    [2] = the caller's caller (the target)
-    */
-    offset = 0
-): CallSite {
+export function getCallSite(offset = 0): CallSite {
     let callsite: CallSite;
     
-    const formatter = ((_: Error, stack: CallStack) => stack) satisfies CallStackFormatter;
-    const temp = Error.prepareStackTrace;
-    Error.prepareStackTrace = formatter;
+    const standardFormatter  = Error.prepareStackTrace;
+    const temporaryFormatter = ((_: Error, stack: CallStack) => stack) satisfies CallStackFormatter;
+    
+    Error.prepareStackTrace = temporaryFormatter;
     {
         const error = new Error();
         const stack = (
-            error.stack as any as ReturnType<typeof formatter>
+            error.stack as any as ReturnType<typeof temporaryFormatter>
         ).slice();
-        
-        callsite = stack[2 + offset] ?? 
-            panic(`Call stack is not deep enough.`)
-        ;
+        /** 
+        [0] = getCallSite  
+        [1] = the caller (who wants to know who called /him/)
+        [2] = the caller's caller (the target)
+        */
+        callsite = stack[offset + 2] ?? panic(
+            `Call stack is not deep enough.`
+        );
     }
-    Error.prepareStackTrace = temp;
+    Error.prepareStackTrace = standardFormatter;
     
     return callsite;
 }
 
 const fileScheme = "file:";
 // TODO: Do some polyfill magic that makes it so the following line never errors
-const fileURLToPath: (url: string) => string = require("url").fileURLToPath;
+// const fileURLToPath: (url: string) => string = require("url").fileURLToPath;
 
 function normalizePath(urlOrPath: string): AbsolutePath {
     // let callerPath: string;
@@ -68,7 +67,7 @@ export function getCallerPath(offset = 0): AbsolutePath {
     // [1] = getCallerFile
     // [2] = the caller (who wants to know who called /him/)  
     // [3] = the caller's caller (the target)
-    const targetCallsite = getCallSite(1 + offset); 
+    const targetCallsite = getCallSite(offset + 1); 
     const urlOrPath = targetCallsite.getFileName() ?? panic("No file name.");
     return normalizePath(urlOrPath);
 }
@@ -79,5 +78,5 @@ export function getCallerPath(offset = 0): AbsolutePath {
  * Consider using {@link getCallerPath} if you only need the path.
  */
 export function getCallerFile(offset = 0): File {
-    return new File(getCallerPath(1 + offset));
+    return new File(getCallerPath(offset + 1));
 }
