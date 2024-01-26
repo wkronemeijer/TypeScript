17149e2fb94f62029efda6cabeb4469d88feafa5
Wessel Kronemeijer <wessel32@gmail.com>
2024-01-26 14:43:29 +0100

    Add defer

---
+++ ts-System/src/Data/Promise/Defer.ts
@@ -0,0 +1,22 @@
+import { swear } from "../../Assert";
+
+interface defer_Result<T> {
+    readonly promise: Promise<T>;
+    readonly resolve: (value: T | PromiseLike<T>) => void;
+    readonly reject : (reason?: unknown) => void;
+}
+
+export function defer<T = void>(): defer_Result<T> {
+    // TODO: Replace with 
+    // return Promise.withResolvers()
+    // ...when it is accepted
+    let resolve: defer_Result<T>["resolve"] | undefined;
+    let reject : defer_Result<T>["reject"]  | undefined;
+    const promise = new Promise<T>((ok, err) => {
+        resolve = ok;
+        reject  = err;
+    });
+    swear(resolve);
+    swear(reject);
+    return { promise, resolve, reject };
+}
