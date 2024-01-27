ecebad8d55844e403b53b20b75367e4fdcdca824
Wessel Kronemeijer <wessel32@gmail.com>
2024-01-27 17:30:31 +0100

    Refactor stylesheet renderer

--- ts-node-tool-JsxServer/src/Domain/Transforms/Client/RenderStylesheet.tsx
+++ ts-node-tool-JsxServer/src/Domain/Transforms/Client/RenderStylesheet.tsx
@@ -1,4 +1,5 @@
 import { fileURLToPath } from "url";
+import { RawSourceMap } from "source-map-js";
 import * as scss from "sass-embedded";
 
 import { AbsolutePath, Path_relative, RelativePath_toUrl } from "@wkronemeijer/system-node";
@@ -7,7 +8,25 @@ import { stringifyJson, swear } from "@wkronemeijer/system";
 import { FileTransform } from "../PageTransform";
 import { CssStylesheet } from "../../ResultTypes/CssStylesheet";
 
+function SourceMap_relativize(self: RawSourceMap, path: AbsolutePath): RawSourceMap {
+    const newSources = self.sources.map(fileUrl => {
+        const source = path;
+        const target = AbsolutePath(fileURLToPath(fileUrl));
+        return RelativePath_toUrl(Path_relative(source, target));
+    });
+    return { ...self, sources: newSources };
+}
+
 const encoding: BufferEncoding = "utf-8";
+
+// Based on https://github.com/sass/dart-sass/issues/1594#issuecomment-1013208452
+function SourceMap_toCssComment(self: RawSourceMap): string {
+    const json       = stringifyJson(self);
+    const base64json = Buffer.from(json, encoding).toString("base64");
+    return `/*# sourceMappingURL=data:application/json;charset=${encoding};base64,${base64json} */`;
+}
+
+const AnsiEscapeSequence = /\x1B\[\d{1,2}m/g;
 
 export const StylesheetRenderer: FileTransform<CssStylesheet> = {
     pattern: /\.scss$/,
@@ -17,40 +36,18 @@ export const StylesheetRenderer: FileTransform<CssStylesheet> = {
             sourceMap: true,
             sourceMapIncludeSources: true,
         });
-        
-        const sourceMap = compileResult.sourceMap;
-        swear(sourceMap, "missing sourcemap");
-        
-        // sass includes absolute URLs by default, we want relative
-        sourceMap.sources = sourceMap.sources.map(fileUrl => {
-            const source        = path;
-            const target        = AbsolutePath(fileURLToPath(fileUrl));
-            const relativePath  = Path_relative(source, target);
-            const relativeUrl   = RelativePath_toUrl(relativePath);
-            return relativeUrl;
-        });
-        
-        // https://github.com/sass/dart-sass/issues/1594#issuecomment-1013208452
-        // embedding source maps has to be done manually
-        
-        const jsonSourceMap        = stringifyJson(sourceMap);
-        const base64JsonSourceMap  = Buffer.from(jsonSourceMap, encoding).toString("base64");
-        const sourceMapComment     = `/*# sourceMappingURL=data:application/json;charset=${encoding};base64,${base64JsonSourceMap} */`;
-        
-        const css = `${compileResult.css}\n\n${sourceMapComment}`;
-        return CssStylesheet(css);
+        const absoluteSourceMap = compileResult.sourceMap;
+        swear(absoluteSourceMap, "missing sourcemap");
+        const relativeSourceMap = SourceMap_relativize(absoluteSourceMap, path);
+        const sourceMapComment  = SourceMap_toCssComment(relativeSourceMap);
+        return CssStylesheet(`${compileResult.css}\n\n${sourceMapComment}`);
     },
-    async renderError_async({error}) {
+    async renderError_async({ error }) {
         const message = (
             error.stack
             ?.replaceAll("*/", "*\u200B/")
-            ?.replaceAll(/\x1B\[\d{1,2}m/g, "")
+            ?.replaceAll(AnsiEscapeSequence, "")
         );
-        return CssStylesheet(`\
-/*
-${message} 
-*/
-body { color: red; }
-`       );
+        return CssStylesheet(`/*\n${message}\n*/\nbody { color: red; }\n`);
     },
 }
