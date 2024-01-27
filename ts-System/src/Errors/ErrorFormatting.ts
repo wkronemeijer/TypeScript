9e811899ec9d80739c33bfc41ac220d7e7f91d7e
Wessel Kronemeijer <wessel32@gmail.com>
2024-01-27 16:32:40 +0100

    Split throwable formatting and logging

--- ts-System/src/Errors/ErrorFormatting.ts
+++ ts-System/src/Errors/ErrorFormatting.ts
@@ -1,19 +1,21 @@
-import { terminal } from "../Logging/Terminal";
 import { Exception } from "../Assert";
+import { terminal } from "../Logging/Terminal";
 
-//////////////////////////////////////////////
-// Standardize when and how to print errors //
-//////////////////////////////////////////////
-/** Prints a thrown value appropriately, i.e. bugs get a stack trace, but exceptions only display the message. */
+export type Throwable = unknown;
 
-export function printError(error: unknown): void {
-    const message =
+/** Formats a thrown value appropriately, i.e. bugs get a stack trace, but exceptions only display the message. */
+export function formatThrowable(error: Throwable) {
+    return (
         // Exceptions and subclasses
         (error instanceof Exception) ? error.message :
         // All other Errors
         (error instanceof Error) ? error.stack :
         // Things that are not errors 
-        String(error)
-    ;
-    terminal.error(message);
+        `Error: ${String(error)}`
+    );
+}
+
+/** Prints a thrown value appropriately, i.e. bugs get a stack trace, but exceptions only display the message. */
+export function printThrowable(error: Throwable): void {
+    terminal.error(formatThrowable(error));
 }
