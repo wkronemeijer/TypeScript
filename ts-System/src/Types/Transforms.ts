a56509d8018087dc9deb66eebd4df83d12e711b4
Wessel Kronemeijer <wessel32@gmail.com>
2024-02-09 20:48:32 +0100

    Expand ReadonylRecord

--- ts-System/src/Types/Transforms.ts
+++ ts-System/src/Types/Transforms.ts
@@ -23,4 +23,6 @@ export type StripNull<T> = T extends null ? never : T;
 export type StripUndefined<T> = T extends undefined ? never : T;
 
 /** Combines {@link Readonly} and {@link Record}. Keeps the number of >>>> down. */
-export type ReadonlyRecord<K extends keyof_t, T> = Readonly<Record<K, T>>;
+export type ReadonlyRecord<K extends keyof_t, T> = { 
+    readonly [P in K]: T; 
+};
