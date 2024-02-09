be20485c115642a23a936e910514ce519fd6c879
Wessel Kronemeijer <wessel32@gmail.com>
2024-02-09 20:48:22 +0100

    Expand IsX

--- ts-System/src/Types/IsX.ts
+++ ts-System/src/Types/IsX.ts
@@ -6,3 +6,11 @@ export function isString(value: unknown): value is string {
 export function isObject(value: unknown): value is object {
     return (typeof value === "object" && value !== null);
 }
+
+export function isFunction(value: unknown): value is Function {
+    return typeof value === "function";
+}
+
+export function isArray(value: unknown): value is unknown[] {
+    return Array.isArray(value);
+}
