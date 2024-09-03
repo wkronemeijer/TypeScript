a01c203a1f4022ae0dc81c52da72f9640a17e105
Wessel Kronemeijer <wessel32@gmail.com>
2024-09-03 14:13:44 +0200

    Add bin.js helper

---
+++ ts-node-lib-NodeCommons/src/IO/CliArgs.ts
@@ -0,0 +1,19 @@
+import {formatThrowable} from "@wkronemeijer/system";
+
+/** Wrapper to run main, meant for `bin.js`. */
+export async function startProgram(
+    main: (args: string[]) => unknown,
+    args: Iterable<string> = process.argv.slice(2),
+): Promise<undefined | Error> {
+    try {
+        await main([...args]);
+    } catch (e) {
+        formatThrowable(e);
+        if (e instanceof Error) {
+            return e;
+        } else {
+            return new Error(String(e));
+        }
+    }
+    return undefined;
+}
