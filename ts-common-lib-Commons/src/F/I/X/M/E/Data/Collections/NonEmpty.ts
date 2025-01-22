cac53d89ad263dffbb6a3b5baa350e3ef3652c33
Wessel Kronemeijer <wessel32@gmail.com>
2025-01-22 12:17:30 +0100

    Forgot a +

--- ts-common-lib-Commons/src/F/I/X/M/E/Data/Collections/NonEmpty.ts
+++ ts-common-lib-Commons/src/F/I/X/M/E/Data/Collections/NonEmpty.ts
@@ -3,8 +3,19 @@
  * 
  * Readonly, because mutating this array can invalidate the type.
  */
-export type NonEmpty<T> = readonly [T, ...T[]];
+export type NonEmpty<T> = readonly [head:T, ...tail: T[]];
 
 export function isNonEmpty<T>(array: readonly T[]): array is NonEmpty<T> {
     return array.length > 0;
+}
+
+export function NonEmpty<T>(array: readonly T[]): NonEmpty<T> {
+    if (!isNonEmpty(array)) {
+        throw new Error("array was empty");
+    }
+    return array;
+}
+
+export function NonEmpty_last<T>(list: NonEmpty<T>): T {
+    return list.at(-1)!;
 }
