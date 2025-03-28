c495fbfc78c1a393482f6373534274c337e2b3e2
Wessel Kronemeijer <wessel32@gmail.com>
2025-03-28 14:40:48 +0100

    Preserve this for useEvent callbacks

--- ts-web-lib-ReactDomCommons/src/Hooks/EventCallback.ts
+++ ts-web-lib-ReactDomCommons/src/Hooks/EventCallback.ts
@@ -1,24 +1,28 @@
-import {useCallback, useLayoutEffect, useRef} from "react";
-import {deprecatedAlias} from "@wkronemeijer/system";
+import {ChangeEvent, useCallback, useLayoutEffect, useRef} from "react";
 
 type AnyFunc = (...args: any[]) => any;
 
 // Based on https://github.com/reactjs/rfcs/blob/useevent/text/0000-useevent.md
 export function useEvent<F extends AnyFunc>(fn: F): F {
+    // TODO: You want to throw if called during render
+    // But you can only detect this the first time, 
+    // afterwards the closure is just outdated.
     const ref = useRef<F | null>(null);
     
-    // TODO: Why do we use `useLayoutEffect` instead of `ref.current = fn`?
-    // As for timing, we can simply say that calling it during render is UB
+    // Q: Why do we use `useLayoutEffect` instead of just `ref.current = fn`?
+    // A: Renders can be discarded by e.g. `Suspense`; 
+    // `useLayoutEffect` on the other hand only triggers for commits.
     useLayoutEffect(() => {
         ref.current = fn;
     }, [fn]);
     
-    const stable = useCallback<AnyFunc>((...args) => {
-        return (void 0, ref.current)!(...args);
+    const stable = useCallback<AnyFunc>(function(this: unknown, ...args) {
+        return ref.current!.apply(this, args);
     }, []);
     
     return stable as F;
 }
 
-/** @deprecated Use the plain {@link useEvent} */
-export const useEventCallback = deprecatedAlias("useEventCallback", useEvent);
+export const useInputEvent = useEvent<(
+    (event: ChangeEvent<HTMLInputElement>) => void
+)>;
